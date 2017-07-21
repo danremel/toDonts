f65b953fcbed3b8379b2e0e81002ce9d2d638d5d
danremel <remel.dan@gmail.com>
2017-07-21 16:43:41 -0400

    added BodyParser. Finished 'new' and 'edit/put' routes.

--- controller/todonts.js
+++ controller/todonts.js
@@ -10,6 +10,51 @@ router.get('/', (req,res) => {
 	});
 });
 
+// New ToDonts
+router.get('/new', (req, res) => {
+	res.render('todonts/new');
+});
 
+
+// Show ToDonts
+router.get('/:id', (req, res) => {
+	const id = req.params.id;
+	const todonts = data.seededToDonts[id];
+	res.render('todonts/show', {
+		todonts: todonts,
+		id: id,
+	});
+});
+
+// Edit ToDonts
+router.get('/:id/edit', (req, res) => {
+	const id = req.params.id;
+	const todonts = data.seededToDonts[id];
+	res.render('todonts/edit', {
+		todonts: todonts,
+		id: id
+	});
+});
+
+// Update ToDonts
+router.put('/:id', (req, res) => {
+	const id = req.params.id;
+	const todonts = data.seededToDonts[id];
+	todonts.description = req.body.description;
+	todonts.urgent = req.body.urgent;
+	res.method = "GET";
+	res.redirect(`/todonts/${id}`);
+});
+
+// Save ToDonts
+router.post('/', (req, res) => {
+	console.log(req.body);
+	const newTodonts = {
+		description: req.body.description,
+		urgent: req.body.urgent
+	};
+	data.seededToDonts.push(newTodonts);
+	res.redirect('/todonts');
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,20 +2,25 @@
 const express = require('express');
 const hbs = require('hbs');
 const logger = require('morgan');
-
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 // App Settings
 const app = express();
 const port = process.env.PORT || 3000;
+
 // Application Params
 const TodontsController = require('./controller/todonts');
+
 // Log from Morgan
 app.use(logger('dev'));
-
-
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(methodOverride('_method'));
 // Views
 app.set('view engine', 'hbs');
-
 app.use('/todonts', TodontsController);
+
 // Home
 app.get('/', (req, res) => {
 	res.send("Home page");
